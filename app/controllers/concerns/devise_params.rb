b9eca799961680eabdb6c580d5a559f7daca488b
Timothy Craig <ttcraig@Timothys-MacBook-Pro.local>
2017-04-10 22:04:14 -0400

    added devise params

---
+++ app/controllers/concerns/devise_params.rb
@@ -0,0 +1,12 @@
+module DeviseParams
+  extend ActiveSupport::Concern
+
+  included do
+    before_filter :configure_permitted_params, if: :devise_controller?
+
+    def configure_permitted_params
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :portfolios, except: [:show]
-  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
 
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+
+  resources :portfolios, except: [:show]
+  get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
   resources :blogs do
     member do
